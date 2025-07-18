fbfbe47d12d4033d3ad3cd945447e94850acdd68
JisusTQ <jesus.torresq@udea.edu.co>
2025-07-18 17:16:08 -0500

    prisma fix

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -6,10 +6,27 @@ declare global {
 }
 let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({
+    // Esta opción es clave para Vercel:
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+    // Evita el uso de prepared statements (workaround seguro para serverless)
+    // Puedes quitar esto si usas Prisma Accelerate
+    log: ['error'],
+  });
 } else {
   if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
+    global.prismaGlobal = new PrismaClient({
+    datasources: {
+      db: {
+        url: process.env.DATABASE_URL,
+      },
+    },
+    log: ['error'],
+  });
   }
   prisma = global.prismaGlobal;
 }
